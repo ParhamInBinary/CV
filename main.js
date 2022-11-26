ab7e9fc53732a0ead98355f181f39c827c94438f
ParhamInBinart <parham.berenjian@gmail.com>
2022-11-26 23:59:56 +0100

    Qualifiacations dropdown complete

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ function main() {
 function addEventListeners() {
     const selectBox = document.querySelector(".selectBox");
     selectBox.addEventListener("click", toggleDropdown);
+
+    const messageMe = document.querySelector(".messageMeBtn");
+    messageMe.addEventListener("click", toggleMessageWindow);
+
+    const messageMeXMark = document.querySelector("#messageMeXMark")
+    messageMeXMark.addEventListener("click", toggleMessageWindow);
+}
+
+
+function toggleMessageWindow() {
+    const messageBox = document.querySelector(".messageBox");
+    messageBox.classList.toggle("show-message-window");
+    
+    const footer = document.querySelector("footer");
+    footer.classList.toggle("displayNone");
 }
 
 function toggleDropdown() {
@@ -28,18 +43,32 @@ function selectOption(e) {
     selected.innerHTML = "";
     selected.append(selectedIcon, selectedText);
     toggleDropdown();
-
-    if ( selected.innerText === "Frontend development") {
-        console.log("asd")
-        
-        const backEnd = document.querySelector("backEnd");
-        const GaD = document.querySelector("GaD");
-
-        backEnd.classList.toggle("skills-hidden")
-        GaD.classList.toggle("skills-hidden")
-        
-
-    }
+    updateSkillResults();
 }
 
-
+function updateSkillResults() {
+    const selectedSkill = document.querySelector(".selected").innerText.trim();
+    const GraphicsAndDesignsResults = document.querySelector(".GraphicsAndDesigns");
+    const backEndResults = document.querySelector(".backEnd");
+    const frontEndResults = document.querySelector(".frontEnd");
+    
+    
+    if ( selectedSkill === "Frontend development") {
+        
+        backEndResults.classList.toggle("displayNone", true);
+        GraphicsAndDesignsResults.classList.toggle("displayNone", true);
+        frontEndResults.classList.toggle("displayNone", false)
+    } 
+    else if ( selectedSkill === "Backend development" ) {
+        
+        backEndResults.classList.toggle("displayNone", false);
+        GraphicsAndDesignsResults.classList.toggle("displayNone", true);
+        frontEndResults.classList.toggle("displayNone", true)
+    }
+    else if ( selectedSkill === "Graphics & design" ) {
+        
+        backEndResults.classList.toggle("displayNone", true);
+        GraphicsAndDesignsResults.classList.toggle("displayNone", false);
+        frontEndResults.classList.toggle("displayNone", true)
+    }
+}
